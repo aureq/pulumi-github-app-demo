07f1ac97a724edf34504fefa16e1a45f51dc6efd
Aurelien Requiem <aurelien@requiem.fr>
2022-11-12 12:27:08 +1100

    comment out ssh sg

--- index.ts
+++ index.ts
@@ -9,36 +9,37 @@ export = async () => {
     const vpc = new acmeVpc.Vpc("vpc", {
         availabilityZones: ["ap-southeast-2a", "ap-southeast-2b"],
         cidrBlock: "10.42.0.0/16",
-        ownerEmail: "aureqw@pulumi.com",
+        ownerEmail: "aureq@pulumi.com",
         region: awsConfig.require("region"),
         subnetMask: "255.255.240.0",
         enableDnsHostnames: true,
         enableDnsSupport: true,
     });
 
-    const sshSg = new aws.ec2.SecurityGroup("aws-sg-allowSsh", {
-        vpcId: vpc.vpc.id,
-        description: "Allow SSH inbound traffic",
-        tags: {
-            'Name': "aws-sg-allowSsh",
-        },
-        ingress: [{
-            cidrBlocks: ['0.0.0.0/0'],
-            fromPort: 22,
-            toPort: 22,
-            protocol: 'tcp',
-            description: 'SSH to VPC'
-        }],
-        egress: [{
-            cidrBlocks: ['0.0.0.0/0'],
-            fromPort: 0,
-            toPort: 0,
-            protocol: '-1'
-        }],
-    }, {
-        parent: vpc
-    });
-    return {
+    // const sshSg = new aws.ec2.SecurityGroup("aws-sg-allowSsh", {
+    //     vpcId: vpc.vpc.id,
+    //     description: "Allow SSH inbound traffic",
+    //     tags: {
+    //         'Name': "aws-sg-allowSsh",
+    //     },
+    //     ingress: [{
+    //         cidrBlocks: ['0.0.0.0/0'],
+    //         fromPort: 22,
+    //         toPort: 22,
+    //         protocol: 'tcp',
+    //         description: 'SSH to VPC'
+    //     }],
+    //     egress: [{
+    //         cidrBlocks: ['0.0.0.0/0'],
+    //         fromPort: 0,
+    //         toPort: 0,
+    //         protocol: '-1'
+    //     }],
+    // }, {
+    //     parent: vpc
+    // });
 
+    return {
+        vpcId: vpc.vpc.id
     }
 }
